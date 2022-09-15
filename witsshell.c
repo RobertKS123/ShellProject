99ca75d267ae541a2d2690545d51ba833a0a3d30
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2022-09-15 14:41:17 +0200

    initial commit

---
+++ witsshell.c
@@ -0,0 +1,181 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <assert.h>
+#include <fcntl.h>
+
+char **path;
+
+bool endLoop(char *arr){
+	const char x[4] = {'e','x','i','t'};
+	bool status = true;
+	if (sizeof arr >= 4){
+		for(int i=0;i<4;i++){
+			if (arr[i] == x[i]){
+				status = false;
+			} else {
+				status = true;
+				break;
+			}
+		}
+	}
+	return status;
+}
+
+char *readLine(){
+	char *buffer;
+	size_t bufsize = 32;
+	size_t characters;
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+    if( buffer == NULL)
+    {
+        perror("Unable to allocate buffer");
+        exit(1);
+    }
+
+	characters = getline(&buffer,&bufsize,stdin);
+	return buffer;
+}
+
+char **splitLine(char *line){
+
+	size_t bufsize = 64;
+	char **instructions =  malloc(bufsize * sizeof(char*));
+	char *found;
+	int p = 0;
+	const char *delim = "\t\r\a\n ";
+
+    while( (found = strsep(&line,delim)) != NULL ){
+		instructions[p] = found; 
+		p++;
+	}
+	instructions[p-1] = NULL;
+
+	return instructions;
+}
+
+char **handlePath(char **args, char **path){
+	if (args[1] == NULL) {
+		path[0] = NULL;
+	} else {
+		int p = 1;
+		while (args[p] != NULL)
+		{
+			path[p-1] = args[p];
+			p++;
+		}
+	}
+	return path;
+}
+
+char *handleOutput(char **args){
+	char *name = NULL;
+	int i = 0;
+	while (args[i] != NULL) {
+		if (strcmp(args[i],">" )==0){
+			if (args[i+2] == NULL) {
+				name = args[i+1];
+				args[i] = NULL;
+				args[i+1] = NULL;
+				//printf("%s\n",name);
+			} else {
+				printf("ERROR\n");
+			}
+		}
+		i++;
+	}
+	return name;
+}
+
+int doInstructions(char **args, char **path){
+	int wpid;
+	int pid;
+	int status = 0;
+
+	if (strcmp(args[0],"path" )==0) {
+		path = handlePath(args,path);
+		return 1;
+	}
+	if (strcmp(args[0],"exit" )==0) {
+		return 0;
+	}
+	if (strcmp(args[0],"cd" )==0) {
+		chdir(args[1]);
+	}
+
+	if (path[0] != NULL){
+		int p = 0;
+		while (path[p] != NULL) {
+			char *temp = (char *) malloc( strlen(path[p]) + 1 );
+			strcpy(temp,path[p]);
+			strcat(temp,args[0]);
+			//printf("%s\n",path[p]);
+			if (access(temp,X_OK) != -1){
+				args[0] = temp;
+				char *fileName = handleOutput(args);
+				if (fileName != NULL) {
+					pid = fork();
+					if (pid == 0){
+						freopen(fileName, "w", stdout); 
+						execv(args[0], args);
+						if (status == -1) {
+							printf("Process did not terminate correctly\n");
+							exit(1);
+						}
+					}
+				} else {
+					pid = fork();
+					if (pid == 0){
+						status = execv(args[0], args); 
+						if (status == -1) {
+							printf("Process did not terminate correctly\n");
+							exit(1);
+						}
+					}
+				while ((wpid = wait(&status)) > 0);
+				}
+			} else {
+				p++;
+			}
+		}
+	}
+	return 1;
+}
+
+void print(char **arr) {
+	int p = 0;
+	char *instruction = arr[p];
+	while (instruction != NULL) {
+		p++;
+		printf("%s\n",instruction);
+		instruction = arr[p];
+	}
+	
+}
+
+int main(int MainArgc, char *MainArgv[]){
+
+	//chdir("/bin/");
+	size_t bufsize = 64;
+	path = malloc(bufsize * sizeof(char*));
+	path[0] = "/bin/";
+
+	bool status = 1;
+	char *line;
+	char **instructions;
+
+	do {
+		printf("witsshell> ");
+		line = readLine();
+		instructions = splitLine(line);
+		status = doInstructions(instructions,path);
+	} while (status);
+	return(0);
+}
+
+
+
